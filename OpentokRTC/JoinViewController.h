2d583ec59814f8416e7eca907af30d03b2639985
andreaphillips <andrea@agilityfeat.com>
2015-09-02 12:30:19 -0600

    added another class

---
+++ OpentokRTC/JoinViewController.h
@@ -0,0 +1,18 @@
+//
+//  JoinViewController.h
+//  OpentokRTC
+//
+//  Created by Andrea Phillips on 02/09/2015.
+//  Copyright (c) 2015 Agilityfeat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JoinViewController : UIViewController
+
+@property (nonatomic, strong) IBOutlet UITextField *userName;
+@property (nonatomic, strong) IBOutlet UITextField *roomName;
+@property (nonatomic, strong) IBOutlet UITextField *roomToken;
+
+
+@end
