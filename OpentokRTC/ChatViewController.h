400baac3969879ea7ba6fe60d269309824cc9e0b
German <german.goldenstein@gmail.com>
2015-09-07 14:21:00 -0300

    chat

---
+++ OpentokRTC/ChatViewController.h
@@ -0,0 +1,10 @@
+//
+//  ChatViewController.h
+//
+
+#import <UIKit/UIKit.h>
+#import "UIBubbleTableViewDataSource.h"
+
+@interface ChatViewController : UIViewController <UIBubbleTableViewDataSource>
+
+@end
